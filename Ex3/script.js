6ae6579130a617b85e45797b247d64724914e568
Frank Osorio <frankosorio4@gmail.com>
2024-02-14 18:07:53 -0300

    adding problems

---
+++ Ex3/script.js
@@ -0,0 +1,10 @@
+// Combine duas strings usando o operador de concatenação e exiba o resultado no console.
+// Utilize o template string ou a concatenação simples.
+
+let word1 = "Hello, ";
+
+let word2 = "Futuro Dev.";
+
+let word3 = word1.concat(word2);
+
+console.log(word3)
