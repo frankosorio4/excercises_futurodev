fc40202c082e461d6c39b3db8e3f07765590b107
Frank Osorio <frankosorio4@gmail.com>
2024-02-14 18:01:42 -0300

    adding problem 2

--- Ex1/script.js
+++ Ex1/script.js
@@ -1,3 +1,6 @@
+// Crie uma função que exiba uma mensagem simples no console.
+// escolha a mensagem que será exibida no console.log
+
 let m = "Hello, Futuro Dev.";
 
 message(m);
---
+++ Ex2/script.js
@@ -0,0 +1,10 @@
+// Declare uma variável e atribua um número a ela, em seguida, exiba seu valor no console.
+// Atribua o valor a variável usando o window.prompt();
+
+let variable1 = "Hola";
+
+console.log(variable1);
+
+let variable2 = window.prompt("Introduce the value of the 2nd variable.");
+
+console.log("The 2nd variable value is: " + variable2);
