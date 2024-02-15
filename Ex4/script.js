44f553a99e8f5006e7ca86538e81d31bc6b3f8d5
Frank Osorio <frankosorio4@gmail.com>
2024-02-15 18:43:47 -0300

    adding problems

---
+++ Ex4/script.js
@@ -0,0 +1,13 @@
+//Crie uma função que receba dois números como parâmetros e retorne sua soma no console.log.
+
+let number1 = 5;
+
+let number2 = 7;
+
+r = adding(number1, number2);
+console.log(r);
+
+function adding(number1,number2){
+    result = number1 + number2;
+    return result;
+}
