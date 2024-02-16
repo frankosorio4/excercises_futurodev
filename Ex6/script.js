7c7e85dfbd9b38b62cfa0bd7c5bc4df1db373f01
Frank Osorio <frankosorio4@gmail.com>
2024-02-16 18:53:06 -0300

    adding ex

---
+++ Ex6/script.js
@@ -0,0 +1,25 @@
+// Crie 3 objetos com propriedades representando informações pessoais (nome, idade, nacionalidade e profissão) e exiba essas informações com um console.log para cada objeto.
+const pessoa1 = {
+    nome: "frank",
+    idade: 37,
+    nacionalidade: "Venezuelano",
+    profissao: "autonomo"
+}
+
+const pessoa2 = {
+    nome: "andres",
+    idade: 36,
+    nacionalidade: "Venezuelano",
+    profissao: "professor"
+}
+
+const pessoa3 = {
+    nome: "kati",
+    idade: 10,
+    nacionalidade: "Venezuelano",
+    profissao: "estudante"
+}
+
+console.log(pessoa1);
+console.log(pessoa2);
+console.log(pessoa3);
